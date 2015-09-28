24fe3a1f490a41ce4af5334f326e39753b233648
paul <psoucy@sciaps.com>
2015-09-28 17:10:16 -0400

    wip

--- gps/Android.mk
+++ gps/Android.mk
@@ -25,7 +25,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libutils libhardware libc
-LOCAL_SRC_FILES := gps2.cpp gps_parser.cpp nmea_parser.l
+LOCAL_SRC_FILES := gps2.cpp gps_parser.cpp nmea_scanner.l
 LOCAL_MODULE := gps.pcm049
 #LOCAL_MODULE_TAGS := optional
 #LOCAL_MODULE_TAGS := debug
